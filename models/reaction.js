4cdb12dd53767baa47f17af70f0bd45caf823d24
wjimh314 <wjimh314@gmail.com>
2022-09-10 16:38:04 -0400

    edits to reaction.js

--- models/reaction.js
+++ models/reaction.js
@@ -1,20 +1,29 @@
-const { schema, types, Schema } = require('mongoose')
+const { schema, types, Schema } = require("mongoose");
 
-const reactionSchema = new Schema (
-    {
-        reactionId: {
-           Type: Schema.types.objectId,
-           default: () => types.objectId(),
-        },
-        ReactionId: {
-            type: String,
-      required: true,
-      maxlength: 50,
-        },
-        {
-            
-        }
+const reactionSchema = new Schema({
+	reactionId: {
+		Type: Schema.types.objectId,
+		default: () => types.objectId(),
+	},
+	ReactionBody: {
+		type: String,
+		required: true,
+		maxlength: 280,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+    createAt: {
+        type: Date,
+        default: Date.now, 
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    tojson: {
+        getters: true,
+    },
+    id: false,
+    
+});
 
-
-
-    })
+module. exports = reactionSchema;
