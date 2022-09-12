83eefa498762a3642913631b6168aecee70554a4
wjimh314 <wjimh314@gmail.com>
2022-09-12 09:08:22 -0400

    edits to userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { Router } = require("express");
+const { route } = require(".");
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+
+} = require("./../controllers/userController");
+
+router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).post(updateUser).delete(removeFriend);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend)
+
+module.exports = router;
